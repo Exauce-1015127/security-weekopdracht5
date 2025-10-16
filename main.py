3df1ace787df504741021dbd265d5d44776f64a9
Exauce-1015127 <1015127@hr.nl>
2025-10-16 23:43:23 +0200

    ADD: Added comments to explain code better DELETE: Removed 'copyToClipboard' function since it was no longer used

--- main.py
+++ main.py
@@ -9,16 +9,20 @@ app.secret_key = 'friesland'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    # Variabelen initialiseren
     encrypted_data = None
     key_hex = None
     decrypted_message = None
 
     if request.method == 'POST':
         if 'encrypt' in request.form:
-            # Gebruiker wil encrypten
+            # Getypte bericht ophalen uit het formulier op de webpagina
+            # Bericht wordt versleuteld met de geïmporteerde encrypt functie
             message = request.form['message']
             nonce, ciphertext, tag, key = encrypt(message)
-            # Encodeer nonce, ciphertext, tag, key in base64 om ze makkelijk te kunnen zetten
+
+            # Encodeer nonce, ciphertext, tag en key in base64 om zeker te zijn dat het
+            # correct wordt verwerkt bij omzetting naar JSON en html weergave
             nonce_b64 = base64.b64encode(nonce).decode('utf-8')
             ciphertext_b64 = base64.b64encode(ciphertext).decode('utf-8')
             tag_b64 = base64.b64encode(tag).decode('utf-8')
@@ -29,12 +33,13 @@ def index():
                 'tag': tag_b64
             }
         elif 'decrypt' in request.form:
-            # Gebruiker wil decrypten
+            # Ingevoerde sleutels ophalen uit het formulier op de webpagina
             key_hex_input = request.form['key']
             nonce_b64 = request.form['nonce']
             ciphertext_b64 = request.form['ciphertext']
             tag_b64 = request.form['tag']
             try:
+                # Zet hex en base64 terug om naar bytes om correct ontsleutelen
                 key = bytes.fromhex(key_hex_input)
                 nonce = base64.b64decode(nonce_b64)
                 ciphertext = base64.b64decode(ciphertext_b64)
@@ -48,9 +53,14 @@ def index():
 @app.route('/download', methods=['POST'])
 def download_data():
     data = request.form.to_dict()
+    # Gegevens uit het encryption proces ophalen
+    # Deze data wordt in JSON formaat opgeslagen
     # Data bevat key, nonce, ciphertext, tag
     json_data = json.dumps(data)
+
     # Maak een in-memory bestand
+    # Hiermee wordt een tijdelijk bestand aangemaakt dat direct wordt gedownload
+    # Daarna wordt het bestand verwijderd en kan het niet meer worden opgevraagd
     buffer = io.BytesIO()
     buffer.write(json_data.encode('utf-8'))
     buffer.seek(0)
